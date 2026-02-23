5a3ca4890a79e8aac3f946c72b2676ccdcc34f69
YT2515 <167419242+yt2515@users.noreply.github.com>
2026-02-23 16:06:55 +0900

    s.k作の中身をコピペ

--- 2_phase/phase3.py
+++ 2_phase/phase3.py
@@ -112,8 +112,45 @@ def main():
         while True:
             try:
                 if phase == 3:
-                    #ここに遠距離フェーズの処理
-                    phase = 4
+                    try:
+                        curr_lat, curr_lon = idokeido()#現在の緯度経度を取得する
+                        prev_lat, prev_lon = curr_lat, curr_lon
+                        if curr_lat is None or curr_lon is None:
+                            print('緯度と経度の取得に失敗しました。')
+                            phase = 4
+                            continue
+                        else:#緯度経度の取得に成功したとき
+                            #まずは、機体が逆さまになっているかどうかを判断する
+                            if gravity[2] < -2.0:#重力加速度のz成分が-2.0未満ならば、機体が逆さまになっていると判断する
+                                is_inverted=True
+                                md.move('w', power=0.7, duration=5, is_inverted=is_inverted, enable_stack_check=False)#逆さまのときは、モーターの回転方向を反転させて前進するようにする
+                            else:#機体が逆さまになっていないと判断する
+                                is_inverted=False
+                                md.move('w', power=0.7, duration=5, is_inverted=is_inverted, enable_stack_check=False)#通常のときは、モーターの回転方向を反転させずに前進するようにする
+                            while True:
+                                curr_lat, curr_lon = idokeido()#緯度経度の取得
+                                if curr_lat is None or curr_lon is None:
+                                    print('緯度と経度の取得に失敗しました。')
+                                    phase = 4
+                                    break
+                                d, ang_rad = calculate_distance_and_angle(curr_lat, curr_lon, prev_lat, prev_lon, GOAL_LAT, GOAL_LON)#現在地と前回地、ゴールの緯度経度から、現在地とゴールの距離と角度を計算する
+                                md.move('w', power=0.7, duration=5, is_inverted=is_inverted, enable_stack_check=False)
+                                #回転の方向と、回転の時間を指定してしていないので、計算方法は後でやります。 time=rad/ω, 左右はang_radの符号で判断する予定。ひとつ前の行に入れよう
+                                prev_lat, prev_lon = curr_lat, curr_lon#後に距離を出すため動く前の地点の緯度経度を保存
+                                if enable_stack_check == True:
+                                    md.move('s', power=0.7, duration=3, is_inverted=is_inverted, enable_stack_check=False)
+                                    md.move('d', power=0.7, duration=1, is_inverted=is_inverted, enable_stack_check=False)
+                                    md.move('w', power=0.7, duration=2, is_inverted=is_inverted, enable_stack_check=False)
+                                    continue
+                                if d < 10: # 距離が10メートル未満ならゴールに近づいたと判断
+                                    print("近距離フェーズに移行します")
+                                    phase = 4
+                                    break
+                    except Exception as e:
+                        print(f"Error in wait phase: {e}")
+                        time.sleep(1)   
+
+                        phase = 4
 
                 time.sleep(0.1)
 
